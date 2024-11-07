0bc7d45d89920bccb46a18db64d2d8a46b368a1d
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2024-11-07 16:17:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,9 @@
 <?php
+@system("clear");
 $t="\n";
 while(true){
 $no = rand(19999999,99999999);
 $nu = rand(19999999,99999999);
-echo "Referensi :".$no,'CENAIDJA',$nu.$t;
+echo "Referensi : ".$no,'CENAIDJA',$nu.$t;
 sleep(15);
 }
